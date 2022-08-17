4767bf122b0bb5f8d142ab1de72e981d0019c4c3
Mark916 <32362952+Mark916@users.noreply.github.com>
2022-08-17 14:57:49 +0800

    网关模块

---
+++ ek-cloud-gateway/src/main/java/com/ek/cloud/ekcloudgateway/EkCloudGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.ek.cloud.ekcloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EkCloudGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EkCloudGatewayApplication.class, args);
+    }
+
+}
